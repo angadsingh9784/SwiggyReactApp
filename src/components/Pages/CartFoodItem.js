55a5c208075b6d5469f85ec29a56df9ba4686e94
Angad Singh <106268549+angadsingh9784@users.noreply.github.com>
2023-11-03 16:12:30 +0530

    Update CartFoodItem.js

--- src/components/Pages/CartFoodItem.js
+++ src/components/Pages/CartFoodItem.js
@@ -25,8 +25,8 @@ const FoodItem = ({ name, description, imageId,category,price }) => {
         //     </div>
         // </li>
         <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-            <div class="flex w-[40%]">
-                <div class="w-[30%]">
+            <div class="flex md:w-[40%] sm:w-[100%]">
+                <div class="md:w-[30%] ">
                     <img  class="rounded-full p-1 h-20 w-52 border " src={CardImgConst+imageId} />
                 </div>
                 <div class="flex flex-col justify-between ml-4 flex-grow">
@@ -36,17 +36,15 @@ const FoodItem = ({ name, description, imageId,category,price }) => {
                 </div>
             </div>
             <div class="flex justify-center w-1/5">
-                <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                <svg class="fill-current text-gray-600 md:w-3 sm:w-0" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
                 </svg>
-
                 <input class="mx-2 border text-center w-8" type="text" value="1" />
-
-                <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+                <svg class="fill-current text-gray-600 md:w-3 sm:w-0" viewBox="0 0 448 512">
                     <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
                 </svg>
             </div>
-            <span class="text-center w-1/5 font-semibold text-sm">{price/100}</span>
-            <span class="text-center w-1/5 font-semibold text-sm">{price/100}</span>
+            <span class=" text-center w-1/5 font-semibold text-sm">{price/100}</span>
+            <span class="md:visible sm:invisible text-center w-1/5 font-semibold text-sm">{price/100}</span>
         </div>
     )
 }
